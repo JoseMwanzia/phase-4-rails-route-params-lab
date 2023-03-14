f3eef5e75c207313a3a4c8868702d7ba6bbc2daa
joseph mwanzia <makamwanzia@gmail.com>
2023-03-15 00:13:03 +0300

    first deliverables

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,10 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show_id
+    show_id = Student.find(1)
+    # show_id = Student.find(2)
+    show_id = Student.find_by(params[:id])
+    render json: show_id
+  end
 end
